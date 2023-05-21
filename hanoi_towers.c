2797bc2599d0dc520b0dc326f1df1120ed21cc35
EgorN7 <95547143+EgorN7@users.noreply.github.com>
2023-05-22 01:15:26 +0300

    Create hanoi

---
+++ hanoi_towers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void hanoi(int n, int i, int k) {
+	if (n == 1) {
+		printf("Move disk 1 from pin %d to %d.\n", i, k);
+	}
+	else {
+		int tmp = 6 - i - k;
+		hanoi(n - 1, i, tmp);
+		printf("Move disk %d from pin %d to %d.\n", n, i, k);
+		hanoi(n - 1, tmp, k);
+	}
+}
+
+
+int main() {
+	hanoi(3, 1, 2);
+	return 0;
+}
